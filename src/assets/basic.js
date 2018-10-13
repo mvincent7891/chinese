c53edecff690f6423acd1b6f6d88a98ebe79fff3
Michael Parlato <michaelvparlato@gmail.com>
2018-10-12 17:25:40 -0700

    new words

--- src/assets/basic.js
+++ src/assets/basic.js
@@ -426,6 +426,133 @@ const basic = {
     kMandarin: "LAO3",
     uvalue: "U+8001",
     string: "老"
+  },
+  "U+4E2D": {
+    kDefinition:
+      "central; center, middle; in the midst of; hit (target); attain",
+    kMandarin: "ZHONG1 ZHONG4",
+    uvalue: "U+4E2D",
+    string: "中"
+  },
+  "U+4ED6": {
+    kDefinition: "other, another; he, she, it",
+    kMandarin: "TA1",
+    uvalue: "U+4ED6",
+    string: "他"
+  },
+  "U+5B57": {
+    kDefinition: "letter, character, word",
+    kMandarin: "ZI4",
+    uvalue: "U+5B57",
+    string: "字"
+  },
+  "U+8AB0": {
+    kDefinition: "who? whom? whose? anyone?",
+    kMandarin: "SHUI2 SHEI2",
+    uvalue: "U+8AB0",
+    string: "誰"
+  },
+  "U+8ACB": {
+    kDefinition: "ask, request; invite; please",
+    kMandarin: "QING3 QING4 QING1 QING2",
+    uvalue: "U+8ACB",
+    string: "請"
+  },
+  "U+8BF7": {
+    kDefinition: "ask, request; invite; please",
+    kMandarin: "QING3 QING4 QING1 QING2",
+    uvalue: "U+8BF7",
+    string: "请"
+  },
+  "U+8C01": {
+    kDefinition: "who? whom? whose? anyone?",
+    kMandarin: "SHUI2 SHEI2",
+    uvalue: "U+8C01",
+    string: "谁"
+  },
+  "U+8CB4": {
+    kDefinition: "expensive, costly, valuable",
+    kMandarin: "GUI4",
+    uvalue: "U+8CB4",
+    string: "貴"
+  },
+  "U+8D35": {
+    kDefinition: "expensive, costly, valuable",
+    kMandarin: "GUI4",
+    uvalue: "U+8D35",
+    string: "贵"
+  },
+  "U+9EBC": {
+    kDefinition: "interrogative final particle; insignificant, small, tiny",
+    kMandarin: "ME5 MO5 MO3 MA5",
+    uvalue: "U+9EBC",
+    string: "麼"
+  },
+  "U+53EB": {
+    kDefinition: "cry, shout; hail, greet, call",
+    kMandarin: "JIAO4",
+    uvalue: "U+53EB",
+    string: "叫"
+  },
+  "U+59D3": {
+    kDefinition: "one's family name; clan, people",
+    kMandarin: "XING4",
+    uvalue: "U+59D3",
+    string: "姓"
+  },
+  "U+60A8": {
+    kDefinition: "honorific for 'you'",
+    kMandarin: "NIN2",
+    uvalue: "U+60A8",
+    string: "您"
+  },
+  "U+82F1": {
+    kDefinition: "petal, flower, leaf; brave, a hero; England, English",
+    kMandarin: "YING1 YANG1",
+    uvalue: "U+82F1",
+    string: "英"
+  },
+  "U+95EE": {
+    kDefinition: "ask about, inquire after",
+    kMandarin: "WEN4",
+    uvalue: "U+95EE",
+    string: "问"
+  },
+  "U+540C": {
+    kDefinition: "same, similar; together with",
+    kMandarin: "TONG2 TONG4",
+    uvalue: "U+540C",
+    string: "同"
+  },
+  "U+540D": {
+    kDefinition: "name, rank, title, position",
+    kMandarin: "MING2",
+    uvalue: "U+540D",
+    string: "名"
+  },
+  "U+554F": {
+    kDefinition: "ask (about), inquire after",
+    kMandarin: "WEN4",
+    uvalue: "U+554F",
+    string: "問"
+  },
+  "U+5979": {
+    kDefinition: "she, her",
+    kMandarin: "TA1",
+    uvalue: "U+5979",
+    string: "她"
+  },
+  "U+6587": {
+    kDefinition: "literature, culture, writing",
+    kMandarin: "WEN2 WEN4",
+    uvalue: "U+6587",
+    string: "文"
+  },
+  "U+7684": {
+    kDefinition: "possessive, adjectival suffix",
+    kMandarin: "DE5 DI2 DI4",
+    uvalue: "U+7684",
+    string: "的"
   }
 };
 
--- src/assets/new.js
+++ src/assets/new.js
@@ -12,125 +12,65 @@ const newWords = {
     uvalue: "U+4E2D",
     string: "中"
   },
-  "U+4ED6": {
-    kDefinition: "other, another; he, she, it",
-    kMandarin: "TA1",
-    uvalue: "U+4ED6",
-    string: "他"
+  "U+5C71": {
+    kDefinition: "mountain, hill, peak",
+    kMandarin: "SHAN1",
+    uvalue: "U+5C71",
+    string: "山"
   },
-  "U+5B57": {
-    kDefinition: "letter, character, word",
-    kMandarin: "ZI4",
-    uvalue: "U+5B57",
-    string: "字"
+  "U+5EA7": {
+    kDefinition: "seat; stand, base",
+    kMandarin: "ZUO4",
+    uvalue: "U+5EA7",
+    string: "座"
   },
-  "U+8AB0": {
-    kDefinition: "who? whom? whose? anyone?",
-    kMandarin: "SHUI2 SHEI2",
-    uvalue: "U+8AB0",
-    string: "誰"
+  "U+6D77": {
+    kDefinition: "sea, ocean; maritime",
+    kMandarin: "HAI3",
+    uvalue: "U+6D77",
+    string: "海"
   },
-  "U+8ACB": {
-    kDefinition: "ask, request; invite; please",
-    kMandarin: "QING3 QING4 QING1 QING2",
-    uvalue: "U+8ACB",
-    string: "請"
+  "U+8FB9": {
+    kDefinition: "edge, margin, side, border",
+    kMandarin: "BIAN1 BIAN5",
+    uvalue: "U+8FB9",
+    string: "边"
   },
-  "U+8BF7": {
-    kDefinition: "ask, request; invite; please",
-    kMandarin: "QING3 QING4 QING1 QING2",
-    uvalue: "U+8BF7",
-    string: "请"
+  "U+9EC4": {
+    kDefinition: "yellow; surname",
+    kMandarin: "HUANG2",
+    uvalue: "U+9EC4",
+    string: "黄"
   },
-  "U+8C01": {
-    kDefinition: "who? whom? whose? anyone?",
-    kMandarin: "SHUI2 SHEI2",
-    uvalue: "U+8C01",
-    string: "谁"
+  "U+56FD": {
+    kDefinition: "nation, country, nation-state",
+    kMandarin: "GUO2",
+    uvalue: "U+56FD",
+    string: "国"
   },
-  "U+8CB4": {
-    kDefinition: "expensive, costly, valuable",
-    kMandarin: "GUI4",
-    uvalue: "U+8CB4",
-    string: "貴"
+  "U+82B1": {
+    kDefinition: "flower; blossoms",
+    kMandarin: "HUA1",
+    uvalue: "U+82B1",
+    string: "花"
   },
-  "U+8D35": {
-    kDefinition: "expensive, costly, valuable",
-    kMandarin: "GUI4",
-    uvalue: "U+8D35",
-    string: "贵"
+  "U+679C": {
+    kDefinition: "fruit; result",
+    kMandarin: "GUO3 KE4 LUO3 WO3",
+    uvalue: "U+679C",
+    string: "果"
   },
-  "U+9EBC": {
-    kDefinition: "interrogative final particle; insignificant, small, tiny",
-    kMandarin: "ME5 MO5 MO3 MA5",
-    uvalue: "U+9EBC",
-    string: "麼"
+  "U+5728": {
+    kDefinition: "be at, in, on; consist in, rest",
+    kMandarin: "ZAI4",
+    uvalue: "U+5728",
+    string: "在"
   },
-  "U+53EB": {
-    kDefinition: "cry, shout; hail, greet, call",
-    kMandarin: "JIAO4",
-    uvalue: "U+53EB",
-    string: "叫"
-  },
-  "U+59D3": {
-    kDefinition: "one's family name; clan, people",
-    kMandarin: "XING4",
-    uvalue: "U+59D3",
-    string: "姓"
-  },
-  "U+60A8": {
-    kDefinition: "honorific for 'you'",
-    kMandarin: "NIN2",
-    uvalue: "U+60A8",
-    string: "您"
-  },
-  "U+82F1": {
-    kDefinition: "petal, flower, leaf; brave, a hero; England, English",
-    kMandarin: "YING1 YANG1",
-    uvalue: "U+82F1",
-    string: "英"
-  },
-  "U+95EE": {
-    kDefinition: "ask about, inquire after",
-    kMandarin: "WEN4",
-    uvalue: "U+95EE",
-    string: "问"
-  },
-  "U+540C": {
-    kDefinition: "same, similar; together with",
-    kMandarin: "TONG2 TONG4",
-    uvalue: "U+540C",
-    string: "同"
-  },
-  "U+540D": {
-    kDefinition: "name, rank, title, position",
-    kMandarin: "MING2",
-    uvalue: "U+540D",
-    string: "名"
-  },
-  "U+554F": {
-    kDefinition: "ask (about), inquire after",
-    kMandarin: "WEN4",
-    uvalue: "U+554F",
-    string: "問"
-  },
-  "U+5979": {
-    kDefinition: "she, her",
-    kMandarin: "TA1",
-    uvalue: "U+5979",
-    string: "她"
-  },
-  "U+6587": {
-    kDefinition: "literature, culture, writing",
-    kMandarin: "WEN2 WEN4",
-    uvalue: "U+6587",
-    string: "文"
-  },
-  "U+7684": {
-    kDefinition: "possessive, adjectival suffix",
-    kMandarin: "DE5 DI2 DI4",
-    uvalue: "U+7684",
-    string: "的"
+  "U+6709": {
+    kDefinition: "have, own, possess; exist",
+    kMandarin: "YOU3 YOU4",
+    uvalue: "U+6709",
+    string: "有"
   }
 };
 export default newWords;
