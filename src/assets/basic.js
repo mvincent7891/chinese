3360cfab66aef1645eb59c363aef176c2238879b
Michael Parlato <michaelvparlato@gmail.com>
2018-11-07 14:00:27 -0800

    update vocab

--- src/assets/basic.js
+++ src/assets/basic.js
@@ -553,6 +553,79 @@ const basic = {
     kMandarin: "DE5 DI2 DI4",
     uvalue: "U+7684",
     string: "的"
+  },
+  "U+4E00": {
+    kDefinition: "one; a, an; alone",
+    kMandarin: "YI1",
+    uvalue: "U+4E00",
+    string: "一"
+  },
+  "U+4E2D": {
+    kDefinition:
+      "central; center, middle; in the midst of; hit (target); attain",
+    kMandarin: "ZHONG1 ZHONG4",
+    uvalue: "U+4E2D",
+    string: "中"
+  },
+  "U+5C71": {
+    kDefinition: "mountain, hill, peak",
+    kMandarin: "SHAN1",
+    uvalue: "U+5C71",
+    string: "山"
+  },
+  "U+5EA7": {
+    kDefinition: "seat; stand, base",
+    kMandarin: "ZUO4",
+    uvalue: "U+5EA7",
+    string: "座"
+  },
+  "U+6D77": {
+    kDefinition: "sea, ocean; maritime",
+    kMandarin: "HAI3",
+    uvalue: "U+6D77",
+    string: "海"
+  },
+  "U+8FB9": {
+    kDefinition: "edge, margin, side, border",
+    kMandarin: "BIAN1 BIAN5",
+    uvalue: "U+8FB9",
+    string: "边"
+  },
+  "U+9EC4": {
+    kDefinition: "yellow; surname",
+    kMandarin: "HUANG2",
+    uvalue: "U+9EC4",
+    string: "黄"
+  },
+  "U+56FD": {
+    kDefinition: "nation, country, nation-state",
+    kMandarin: "GUO2",
+    uvalue: "U+56FD",
+    string: "国"
+  },
+  "U+82B1": {
+    kDefinition: "flower; blossoms",
+    kMandarin: "HUA1",
+    uvalue: "U+82B1",
+    string: "花"
+  },
+  "U+679C": {
+    kDefinition: "fruit; result",
+    kMandarin: "GUO3 KE4 LUO3 WO3",
+    uvalue: "U+679C",
+    string: "果"
+  },
+  "U+5728": {
+    kDefinition: "be at, in, on; consist in, rest",
+    kMandarin: "ZAI4",
+    uvalue: "U+5728",
+    string: "在"
+  },
+  "U+6709": {
+    kDefinition: "have, own, possess; exist",
+    kMandarin: "YOU3 YOU4",
+    uvalue: "U+6709",
+    string: "有"
   }
 };
 
--- src/assets/new.js
+++ src/assets/new.js
@@ -1,46 +1,117 @@
 const newWords = {
-  "U+4E00": {
-    kDefinition: "one; a, an; alone",
-    kMandarin: "YI1",
-    uvalue: "U+4E00",
-    string: "一"
+  "U+4E66": {
+    kDefinition: "book, letter, document; writings",
+    kMandarin: "SHU1",
+    uvalue: "U+4E66",
+    string: "书"
   },
-  "U+4E2D": {
-    kDefinition:
-      "central; center, middle; in the midst of; hit (target); attain",
-    kMandarin: "ZHONG1 ZHONG4",
-    uvalue: "U+4E2D",
-    string: "中"
+  "U+4E86": {
+    kDefinition: "to finish; particle of completed action; duile - by the way",
+    kMandarin: "LE5 LIAO3",
+    uvalue: "U+4E86",
+    string: "了"
   },
-  "U+5C71": {
-    kDefinition: "mountain, hill, peak",
-    kMandarin: "SHAN1",
-    uvalue: "U+5C71",
-    string: "山"
+  "U+4F1A": {
+    kDefinition: "can, be able to, know how to",
+    kMandarin: "HUI4 KUAI4 GUI4",
+    uvalue: "U+4F1A",
+    string: "会"
   },
-  "U+5EA7": {
-    kDefinition: "seat; stand, base",
-    kMandarin: "ZUO4",
-    uvalue: "U+5EA7",
-    string: "座"
+  "U+5BF9": {
+    kDefinition: "correct, right; facing, opposed; duile - by the way",
+    kMandarin: "DUI4",
+    uvalue: "U+5BF9",
+    string: "对"
   },
-  "U+6D77": {
-    kDefinition: "sea, ocean; maritime",
-    kMandarin: "HAI3",
-    uvalue: "U+6D77",
-    string: "海"
+  "U+5C11": {
+    kDefinition: "few, less, inadequate",
+    kMandarin: "SHAO3 SHAO4",
+    uvalue: "U+5C11",
+    string: "少"
   },
-  "U+8FB9": {
-    kDefinition: "edge, margin, side, border",
-    kMandarin: "BIAN1 BIAN5",
-    uvalue: "U+8FB9",
-    string: "边"
+  "U+5F88": {
+    kDefinition: "very, quite, much",
+    kMandarin: "HEN3",
+    uvalue: "U+5F88",
+    string: "很"
   },
-  "U+9EC4": {
-    kDefinition: "yellow; surname",
-    kMandarin: "HUANG2",
-    uvalue: "U+9EC4",
-    string: "黄"
+  "U+5FB7": {
+    kDefinition: "ethics, morality, virtue; German, Germany",
+    kMandarin: "DE2",
+    uvalue: "U+5FB7",
+    string: "德"
+  },
+  "U+5FD9": {
+    kDefinition: "busy, pressed for time; hustling",
+    kMandarin: "MANG2",
+    uvalue: "U+5FD9",
+    string: "忙"
+  },
+  "U+6CD5": {
+    kDefinition: "law, rule, regulation, statute; France, French",
+    kMandarin: "FA3",
+    uvalue: "U+6CD5",
+    string: "法"
+  },
+  "U+7A0B": {
+    kDefinition: "journey, trip; schedule, agenda; engineering - gongcheng",
+    kMandarin: "CHENG2",
+    uvalue: "U+7A0B",
+    string: "程"
+  },
+  "U+7CFB": {
+    kDefinition: "major, college, department; system; line, link, connection",
+    kMandarin: "XI4 JI4",
+    uvalue: "U+7CFB",
+    string: "系"
+  },
+  "U+7D2F": {
+    kDefinition: "tired; implicate, involve; bother",
+    kMandarin: "LEI4 LEI3 LEI2 LÜ4 LIE4",
+    uvalue: "U+7D2F",
+    string: "累"
+  },
+  "U+7F8E": {
+    kDefinition: "beautiful, pretty; pleasing; America",
+    kMandarin: "MEI3",
+    uvalue: "U+7F8E",
+    string: "美"
+  },
+  "U+8A00": {
+    kDefinition: "words, speech; speak, say",
+    kMandarin: "YAN2 YAN4 YIN2",
+    uvalue: "U+8A00",
+    string: "言"
+  },
+  "U+8A9E": {
+    kDefinition: "language, words; saying, expression",
+    kMandarin: "YU3 YU4",
+    uvalue: "U+8A9E",
+    string: "語"
+  },
+  "U+8BF4": {
+    kDefinition: "speak, say, talk; scold, upbraid",
+    kMandarin: "SHUO1 SHUI4 TUO1 YUE4",
+    uvalue: "U+8BF4",
+    string: "说"
+  },
+  "U+8FD9": {
+    kDefinition: "this, the, here",
+    kMandarin: "ZHE4 ZHEI4",
+    uvalue: "U+8FD9",
+    string: "这"
+  },
+  "U+53EF": {
+    kDefinition: "may, can, -able; possibly; keshi - however, but",
+    kMandarin: "KE3 KE4",
+    uvalue: "U+53EF",
+    string: "可"
+  },
+  "U+54EA": {
+    kDefinition: "which? where? how?",
+    kMandarin: "NA3 NEI3 NA5 NE2",
+    uvalue: "U+54EA",
+    string: "哪"
   },
   "U+56FD": {
     kDefinition: "nation, country, nation-state",
@@ -48,23 +119,71 @@ const newWords = {
     uvalue: "U+56FD",
     string: "国"
   },
-  "U+82B1": {
-    kDefinition: "flower; blossoms",
-    kMandarin: "HUA1",
-    uvalue: "U+82B1",
-    string: "花"
+  "U+65E5": {
+    kDefinition: "sun; day; daytime; Japan, Japanese",
+    kMandarin: "RI4 MI4",
+    uvalue: "U+65E5",
+    string: "日"
   },
-  "U+679C": {
-    kDefinition: "fruit; result",
-    kMandarin: "GUO3 KE4 LUO3 WO3",
-    uvalue: "U+679C",
-    string: "果"
+  "U+82F1": {
+    kDefinition: "petal, flower, leaf; brave, a hero; England, English",
+    kMandarin: "YING1 YANG1",
+    uvalue: "U+82F1",
+    string: "英"
   },
-  "U+5728": {
-    kDefinition: "be at, in, on; consist in, rest",
-    kMandarin: "ZAI4",
-    uvalue: "U+5728",
-    string: "在"
+  "U+90A3": {
+    kDefinition: "that, that one, those",
+    kMandarin: "NA4 NEI4 NA1 NA3 NE2 NEI3 NUO2 NUO4",
+    uvalue: "U+90A3",
+    string: "那"
+  },
+  "U+96BE": {
+    kDefinition: "difficult, arduous, hard; unable",
+    kMandarin: "NAN2 NAN4 NUO2",
+    uvalue: "U+96BE",
+    string: "难"
+  },
+  "U+513F": {
+    kDefinition: "son, child; KangXi radical 10",
+    kMandarin: "ER2 ER5",
+    uvalue: "U+513F",
+    string: "儿"
+  },
+  "U+529F": {
+    kDefinition: "achievement, merit, good result; gongke - homework",
+    kMandarin: "GONG1",
+    uvalue: "U+529F",
+    string: "功"
+  },
+  "U+548C": {
+    kDefinition: "and; harmony, peace; peaceful, calm",
+    kMandarin: "HE2 HUO4 HUO2 HE4",
+    uvalue: "U+548C",
+    string: "和"
+  },
+  "U+591A": {
+    kDefinition: "much, many; more than, over",
+    kMandarin: "DUO1",
+    uvalue: "U+591A",
+    string: "多"
+  },
+  "U+592A": {
+    kDefinition: "very, too, much; big; extreme",
+    kMandarin: "TAI4",
+    uvalue: "U+592A",
+    string: "太"
+  },
+  "U+672C": {
+    kDefinition: "root, origin, source; basis",
+    kMandarin: "BEN3",
+    uvalue: "U+672C",
+    string: "本"
+  },
+  "U+5801": {
+    kDefinition: "gongke - homework, assignment",
+    kMandarin: "KE4",
+    uvalue: "U+5801",
+    string: "堁"
   },
   "U+6709": {
     kDefinition: "have, own, possess; exist",
